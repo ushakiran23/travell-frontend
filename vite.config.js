8e17bf500b9a4bd21db964b7741b10ddb4f6661b
ushakiran23 <ushakirankoppada8883@gmail.com>
2025-09-22 11:46:28 +0530

    mid-commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/travell/' // Must match your repo name exactly
-});
+  base: '/travell-frontend/',  // important for GitHub Pages
+})
